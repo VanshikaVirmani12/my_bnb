2012a5c4d0887afbe056585ab722a7b2b675230e
axxaxya <annanya21sharma@gmail.com>
2022-07-31 21:10:12 -0400

    Skeleton functions for filters

--- src/main/java/Filter.java
+++ src/main/java/Filter.java
@@ -1,7 +1,9 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
+import java.sql.ResultSet;
 
 public class Filter {
 
@@ -19,8 +21,10 @@ public class Filter {
   Boolean has_washer;
   Boolean has_dryer;
   Boolean has_ac;
+  Integer longitude;
+  Integer latitude;
 
-  public Filter() {
+  public Filter() throws SQLException {
     this.city = city;
     this.country = country;
     this.apt_name = apt_name;
@@ -35,6 +39,8 @@ public class Filter {
     this.has_washer = has_washer;
     this.has_dryer = has_dryer;
     this.has_ac = has_ac;
+    this.longitude = longitude;
+    this.latitude = latitude;
 
   }
 
@@ -46,20 +52,81 @@ public class Filter {
   //Create jdbc_demo table
   String sqlQ;
 
-  public boolean filter_by_address(String city, String country, String apt_name) {
-    if (this.city.equalsIgnoreCase(city) && this.country.equalsIgnoreCase(country) &&
-            this.apt_name.equalsIgnoreCase(apt_name)) {
-      return true;
-    }
-    return false;
-  }
+//  public boolean filter_by_address(String city, String country, String apt_name) {
+//    if (this.city.equalsIgnoreCase(city) && this.country.equalsIgnoreCase(country) &&
+//            this.apt_name.equalsIgnoreCase(apt_name)) {
+//      return true;
+//    }
+//    return false;
+//  }
 
-  public filter_by_price_range(int price_low, int price_high) {
+  public void filter_by_price_range(int price_low, int price_high) throws SQLException {
     sqlQ = "SELECT * from Calender\n" +
             "\tWHERE price >= price_low AND price <= price_high\n" +
             "\tORDER BY price\n";
     System.out.println("Executing this filter_by_price_range: \n" + sqlQ.replaceAll("\\s+", " ") + "\n");
-    sql.executeUpdate(sqlQ);
+    ResultSet rs = sql.executeQuery(sqlQ);
+
+    while (rs.next()){
+      System.out.print("City:"+rs.getString("City") +", ");
+      // Print the data
+    }
+
   }
+
+  public void filter_by_long_lat(int log, int lat, int distance) throws SQLException{
+    // if distance not defined, define a default
+    // order by distance
+    sqlQ = "";
+    System.out.println("Executing this filter_by_long_lat: \n" + sqlQ.replaceAll("\\s+", " ") + "\n");
+    ResultSet rs = sql.executeQuery(sqlQ);
+
+    while (rs.next()){
+      // Print the data
+    }
+  }
+
+  public void filter_by_postal_code(String postal_code) throws SQLException{
+    // same or adjacent postal codes
+    sqlQ = "";
+    System.out.println("Executing this filter_by_postal_code: \n" + sqlQ.replaceAll("\\s+", " ") + "\n");
+    ResultSet rs = sql.executeQuery(sqlQ);
+
+    while (rs.next()){
+      // Print the data
+    }
+  }
+
+  public void filter_by_address(String apt_name, String city, String country, String postal_code) throws SQLException {
+    sqlQ = "";
+    System.out.println("Executing this filter_by_address: \n" + sqlQ.replaceAll("\\s+", " ") + "\n");
+    ResultSet rs = sql.executeQuery(sqlQ);
+
+    while (rs.next()){
+      // Print the data
+    }
+  }
+
+  public void filter_by_date_range(Date start_date, Date end_date) throws SQLException {
+    sqlQ = "";
+    System.out.println("Executing this filter_by_date_range: \n" + sqlQ.replaceAll("\\s+", " ") + "\n");
+    ResultSet rs = sql.executeQuery(sqlQ);
+
+    while (rs.next()){
+      // Print the data
+    }
+  }
+
+  public void filter_by_amenities(boolean has_ac, boolean has_dryer, boolean has_kitchen, boolean has_washer, boolean has_wifi) throws SQLException {
+    sqlQ = "";
+    System.out.println("Executing this filter_by_amenities: \n" + sqlQ.replaceAll("\\s+", " ") + "\n");
+    ResultSet rs = sql.executeQuery(sqlQ);
+
+    while (rs.next()){
+      // Print the data
+    }
+  }
+
+
 
 }
