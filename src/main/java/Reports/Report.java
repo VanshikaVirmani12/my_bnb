2b75f439190ce65466e0f912d4aaf33271491ea3
axxaxya <annanya21sharma@gmail.com>
2022-08-08 00:46:35 -0400

    Added code for report

--- src/main/java/Reports/Report.java
+++ src/main/java/Reports/Report.java
@@ -30,11 +30,13 @@ public class Report {
   public static String endDate;
   private static int wifi, washer, ac, kitchen, dryer;
   private static String[] room_types = {"Apartment", "House", "Room"};
+  static Statement sql;
 
   /*
   Total number of bookings in a specific date range by city.
 Total number of bookings in a specific date range by postal code within a city.
 Total number of listings per country, per country and city, per country, city and postal code.
+
 Rank the hosts by the total number of listings they have overall per country.
 Rank the hosts by the total number of listings they have overall per city.
 For every city and country a report should provide the hosts that have a number of listings that is more than 10% of the number of listings in that city and country.
@@ -54,7 +56,83 @@ A report that presents for each listing the set of most popular noun phrases ass
     System.out.print("Specify city ");
     city = scan.nextLine();
 
+    String sqlQ = "select count(*) from bookings as b join listings as l on l.listing_id = b.listing_id where start = '"+ startDate+ "' AND end = '" + endDate+ "' AND city = '" + city+"'\n";
 
+    rs = sql.executeQuery(sqlQ);
+
+    while(rs.next()){
+      System.out.print("Total number of bookings between "+startDate+ " and " + endDate+" in "+city+ "is: " + rs.getInt("count(*)"));
+    }
 
   }
+
+  public static void dateRangeAndCityAndPostalCode() throws SQLException, InterruptedException {
+    System.out.print("Specify the date range\n");
+    System.out.print("Start Date (YYYY-MM-DD): ");
+    startDate = scan.nextLine();
+    System.out.print("End Date (YYYY-MM-DD): ");
+    endDate = scan.nextLine();
+
+    System.out.print("Specify city ");
+    city = scan.nextLine();
+
+    System.out.print("Specify postal code ");
+    zipcode = scan.nextLine();
+
+    String sqlQ = "select count(*) from bookings as b join listings as l on l.listing_id = b.listing_id where start = '"+ startDate+ "' AND end = '" + endDate+ "' AND city = '" + city+"' AND postal_code = '"+ zipcode+"'\n";
+
+    rs = sql.executeQuery(sqlQ);
+
+    while(rs.next()){
+      System.out.print("Total number of bookings between "+startDate+ " and " + endDate+" in "+city+ " with postal code "+ zipcode+ " is: " + rs.getInt("count(*)"));
+    }
+
+  }
+
+  public static void listingsPerCountryCityPostalCode() throws SQLException, InterruptedException {
+    System.out.print("Enter 1 to get listings per Country, 2 for listings per country and city and 3 for listings per country and city and postal code: ");
+    int choice = scan.nextInt();
+    if (choice == 1){
+      System.out.print("Specify country: ");
+      country = scan.nextLine();
+      String sqlQ = "select count(*) from listings where country = '"+ country+ "'\n";
+      rs = sql.executeQuery(sqlQ);
+      while(rs.next()){
+        System.out.print("Total number of listings for country "+ country + " is: " + rs.getInt("count(*)"));
+      }
+    }else if( choice == 2){
+      System.out.print("Specify country: ");
+      country = scan.nextLine();
+      System.out.print("Specify city: ");
+      city = scan.nextLine();
+      String sqlQ = "select count(*) from listings where country = '"+ country+ "' AND city = '" +city +"'\n";
+      rs = sql.executeQuery(sqlQ);
+      while(rs.next()){
+        System.out.print("Total number of listings for country "+ country + " and city " + city+ " is: " + rs.getInt("count(*)"));
+      }
+    }else{
+      System.out.print("Specify country: ");
+      country = scan.nextLine();
+      System.out.print("Specify city: ");
+      city = scan.nextLine();
+      System.out.print("Specify postal code: ");
+      zipcode = scan.nextLine();
+      String sqlQ = "select count(*) from listings where country = '"+ country+ "' AND city = '" +city +"' AND postal_code = '" + zipcode+"'\n";
+      rs = sql.executeQuery(sqlQ);
+      while(rs.next()){
+        System.out.print("Total number of listings for country "+ country + " and city " + city+ " with postal code " + zipcode +" is: " + rs.getInt("count(*)"));
+      }
+    }
+
+  }
+
+
+  public static void rankHosts(){
+
+  }
+
+
+
+
+
   }
